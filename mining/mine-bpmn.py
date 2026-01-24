1c3504ef8a6cda91fde8e67b635097fd4ccca509
Fabian Strauch <fabian.strauch.04@web.de>
2026-01-24 14:26:48 +0100

    bpmn export to xml string function + standard return as xml string for mining functions

--- mining/mine-bpmn.py
+++ mining/mine-bpmn.py
@@ -1,16 +1,38 @@
 import pm4py as pm
 
-def get_bpmn_heuristic(data):
+"""
+Mines a log with the heuristic miner and returns the bpmn model
+Either as pm4py bpmn type or as bpmn 2.0 xml string
+"""
+def get_bpmn_heuristic(data, as_xml_string = True):
     net, im, fm = pm.discover_petri_net_heuristics(data)
 
     bpmn = pm.convert_to_bpmn(net, im, fm)
 
-    return bpmn
+    if(as_xml_string):
+        return get_xml_string(bpmn)
+    else:
+        return bpmn
 
-def get_bpmn_inductive(data, noise_threshold = 0.0):
+"""
+Mines a log with the inductive miner and returns the bpmn model
+Either as pm4py bpmn type or as bpmn 2.0 xml string
+"""
+def get_bpmn_inductive(data, noise_threshold = 0.0, as_xml_string = True):
     bpmn = pm.discover_bpmn_inductive(
         log = data,
         noise_threshold = noise_threshold
     )
 
-    return bpmn
+    if(as_xml_string):
+        return get_xml_string(bpmn)
+    else:
+        return bpmn
+
+"""
+Serializes a bpmn pm4py bpmn type model into a standard bpmn 2.0 xml string
+that bpmn-js can directly read
+"""
+def get_xml_string(bpmn_model):
+    _, xml_bytes = pm.serialize(bpmn_model)  
+    return xml_bytes.decode('utf-8')
