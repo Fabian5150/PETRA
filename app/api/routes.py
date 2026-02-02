8e6cadd1cf83a2bd1a662795c85b0ab0e1f2d18e
Fabian Strauch <fabian.strauch.04@web.de>
2026-02-02 14:59:16 +0100

    Change bpmn endpoint to plain text

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
 from services.state_service import load_kpis, load_bpmn
 
 router = APIRouter()
 
-@router.get("/process-model")
+@router.get("/process-model", response_class=PlainTextResponse)
 def get_current_process_model():
     data = load_bpmn()
 
-    return {
-        "bpmn": data
-    }
+    return data
 
 @router.get("/kpis")
 def get_current_kpis():
