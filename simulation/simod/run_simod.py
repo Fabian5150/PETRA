7c3bc899647c8daac862d8149a11f6cf295f0330
Fabian Strauch <fabian.strauch.04@web.de>
2026-02-12 22:49:00 +0100

    copy simod files from playground and update venv & requirements to python 3.9

---
+++ simulation/simod/run_simod.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from simod.event_log.event_log import EventLog
+from simod.settings.simod_settings import SimodSettings
+from simod.simod import Simod
+
+def run_simod(log_path: str, config_path: str, output_dir: str):
+    output = Path(output_dir)
+    output.mkdir(parents=True, exist_ok=True)
+    
+    settings = SimodSettings.from_path(Path(config_path))
+    settings.common.train_log_path = Path(log_path)
+    
+    event_log = EventLog.from_path(
+        log_ids=settings.common.log_ids,
+        train_log_path=settings.common.train_log_path,
+        test_log_path=settings.common.test_log_path,
+        preprocessing_settings=settings.preprocessing,
+        need_test_partition=settings.common.perform_final_evaluation,
+    )
+    
+    simod = Simod(settings=settings, event_log=event_log, output_dir=output)
+    simod.run()
+    
+    print(f"simod output in: {output}")
+
+run_simod(
+    log_path="simulation/2012_start_end_times.csv",
+    config_path="simulation/simod_config.yml",
+    output_dir="./simod_output"
+)
