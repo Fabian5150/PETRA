00ec1cf64bc03e671d90bb8ec6b4cffee29630a1
Fabian Strauch <fabian.strauch.04@web.de>
2026-02-12 21:57:38 +0100

    updated import string for 2012 log

--- pre_processing/import_data.py
+++ pre_processing/import_data.py
@@ -1,16 +1,7 @@
-import pm4py as pm
+from pathlib import Path
+import pandas as pd
 
 def import_2012():
-    log = pm.read_xes("/home/fabian/gitProjects/uni/ba/petra/data/BPI-Challenge-2012_1_all/BPI_Challenge_2012.xes")
-
-    # translation of dutch activity names
-    activity_names = list(log["concept:name"].unique())
-    translated_names = ["A_SUBMITTED", "A_PARTLYSUBMITTED", "A_PREACCEPTED", "W_Complete_application", "A_ACCEPTED", "O_SELECTED", "A_FINALIZED", "O_CREATED", "O_SENT", "W_Follow_up_quotations", "O_SENT_BACK", "W_Validate_application", "A_REGISTERED", "A_APPROVED", "O_ACCEPTED", "A_ACTIVATED", "O_CANCELLED", "W_Change_contract_details", "A_DECLINED", "A_CANCELLED", "W_Process_leads", "O_DECLINED", "W_Follow_up_incomplete_files", "W_Assess_fraud"]
-
-    translations = dict(zip(activity_names, translated_names))
-
-    log["concept:name"] = log["concept:name"].map(lambda name : translations[name])
-
-    log.drop(["case:AMOUNT_REQ", "case:REG_DATE"], axis=1)
+    log = pd.read_csv(Path(__file__).parent / ".." / "data" / "bpi_2012" / "bpi_2012_translated.csv")
 
     return log
