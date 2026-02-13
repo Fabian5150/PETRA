22574a1a03a50d6f8e0608179c5e8deb9e6738cd
Fabian Strauch <fabian.strauch.04@web.de>
2026-02-13 14:34:49 +0100

    god only know why this works now, but I am deeply thankful for it

--- pre_processing/approx_start_end_times.py
+++ pre_processing/approx_start_end_times.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
 """
-Takes a log with a lifecylce:transition column
+Takes a log with a transition column
 to approximate missing start and end times for each activity
 TODO: Refactor this mess
 """
 def approx_start_end_times(log, start_lifecycle_name = "START", end_lifecycle_name = "COMPLETE"):
-    start_entries = log[log["lifecycle:transition"] == start_lifecycle_name].sort_values(by="time:timestamp")
-    end_entries = log[log["lifecycle:transition"] == end_lifecycle_name].sort_values(by="time:timestamp")
+    start_entries = log[log["transition"] == start_lifecycle_name].sort_values(by="timestamp")
+    end_entries = log[log["transition"] == end_lifecycle_name].sort_values(by="timestamp")
 
     # Get all activity types which are in end_entries, but not in start_entries or schedule_entries
-    start_activity_types = set(start_entries["concept:name"].unique())
-    end_activity_types = set(end_entries["concept:name"].unique())
+    start_activity_types = set(start_entries["activity_key"].unique())
+    end_activity_types = set(end_entries["activity_key"].unique())
 
     unmatched_activity_types = list(end_activity_types - start_activity_types)
 
-    start_case_grouping = start_entries.groupby("case:concept:name")
+    start_case_grouping = start_entries.groupby("case_id")
     start_case_ids = list(start_case_grouping.groups.keys())
 
-    end_case_grouping = end_entries.groupby("case:concept:name")
+    end_case_grouping = end_entries.groupby("case_id")
 
     start_end_log = []
     unmatched_entries = [] # unmatched start or end entries => approximate their corresponding start/end date
@@ -26,11 +26,11 @@ def approx_start_end_times(log, start_lifecycle_name = "START", end_lifecycle_na
     # merge start and end entries into one activity with start and end timestamps
     def merge_start_end (start, end):
         event = dict()
-        event["concept:name"] = start["concept:name"]
-        event["org:resource"] = start["org:resource"]
-        event["case:concept:name"] = start["case:concept:name"]
-        event["start_date"] = start["time:timestamp"]
-        event["end_date"] = end["time:timestamp"]
+        event["activity_key"] = start.activity_key
+        event["resource"] = start.resource
+        event["case_id"] = start.case_id
+        event["start_date"] = start.timestamp
+        event["end_date"] = end.timestamp
 
         return event
 
@@ -41,9 +41,9 @@ def approx_start_end_times(log, start_lifecycle_name = "START", end_lifecycle_na
         # Find the corresponding end activiy for each start activity
         for start in case_starts:
             end = next((entry for entry in case_ends if (
-                entry["concept:name"] == start["concept:name"] 
-                and entry["org:resource"] == start["org:resource"] # fair to assume this, right?
-                and entry["time:timestamp"] >= start["time:timestamp"]
+                entry.activity_key == start.activity_key 
+                and entry.resource == start.resource
+                and entry.timestamp >= start.timestamp
             )), None)
 
             if(end == None):
@@ -56,23 +56,26 @@ def approx_start_end_times(log, start_lifecycle_name = "START", end_lifecycle_na
 
     real_log = pd.DataFrame(start_end_log)
 
+    real_log["start_date"] = pd.to_datetime(real_log["start_date"], format="ISO8601")
+    real_log["end_date"] = pd.to_datetime(real_log["end_date"], format="ISO8601")
+
     # Aggregate average cycle times per activity type
     activity_cycle_times = real_log.assign(cycle_time = lambda entry : entry["end_date"] - entry["start_date"])
-    average_cycle_times = activity_cycle_times.groupby("concept:name")["cycle_time"].mean()
+    average_cycle_times = activity_cycle_times.groupby("activity_key")["cycle_time"].mean()
 
     # Approximate start / end times of unmatched entries with types, where an average could be determined
     # If it can't be approxiamted, assume it's an automatic activity => start time = end time
     def create_event (entry, approx_start = None, approx_end = None):
         event = dict()
-        event["concept:name"] = entry["concept:name"]
-        event["org:resource"] = entry["org:resource"]
-        event["case:concept:name"] = entry["case:concept:name"]
+        event["activity_key"] = entry.activity_key
+        event["resource"] = entry.resource
+        event["case_id"] = entry.case_id
         
         if(approx_start):
             event["start_date"] = approx_start
-            event["end_date"] = entry["time:timestamp"]
+            event["end_date"] = entry.timestamp
         elif(approx_end):
-            event["start_date"] = entry["time:timestamp"]
+            event["start_date"] = entry.timestamp
             event["end_date"] = approx_end
 
         return event
@@ -80,16 +83,16 @@ def approx_start_end_times(log, start_lifecycle_name = "START", end_lifecycle_na
     start_end_log = []
 
     for entry in unmatched_entries:
-        if(entry.transition == end_lifecycle_name): # end entry
-            if(entry["concept:name"] in start_activity_types):
+        if(entry.transition == "COMPLETE"): # end entry
+            if(entry.activity_key in start_activity_types):
                 start_end_log.append(
-                    create_event(entry = entry, approx_start = entry["time:timestamp"] - average_cycle_times[entry["concept:name"]])
+                    create_event(entry = entry, approx_start = pd.to_datetime(entry.timestamp) - average_cycle_times[entry.activity_key])
                 )
             else:
-                start_end_log.append(create_event(entry = entry, approx_start = entry["time:timestamp"])) # => Automatic event
+                start_end_log.append(create_event(entry = entry, approx_start = entry.timestamp)) # => Automatic event
         else: # start entry
             start_end_log.append(
-                create_event(entry = entry, approx_end = entry["time:timestamp"] + average_cycle_times[entry["concept:name"]])
+                create_event(entry = entry, approx_end = pd.to_datetime(entry.timestamp) + average_cycle_times[entry.activity_key])
             )
 
     approx_log = pd.DataFrame(start_end_log)
@@ -98,6 +101,6 @@ def approx_start_end_times(log, start_lifecycle_name = "START", end_lifecycle_na
     log = pd.concat([real_log, approx_log], axis=0)
 
     # Assign all entries without resource to a global orphanage resource
-    log["org:resource"] = log["org:resource"].fillna("GLOBAL")
+    log["resource"] = log["resource"].fillna("GLOBAL")
 
     return log
--- pre_processing/import_data.py
+++ pre_processing/import_data.py
@@ -4,4 +4,14 @@ import pandas as pd
 def import_2012():
     log = pd.read_csv(Path(__file__).parent / ".." / "data" / "bpi_2012" / "bpi_2012_translated.csv")
 
-    return log.drop(["case:AMOUNT_REQ", "case:REG_DATE"], axis=1)
+    log = log.drop(["case:AMOUNT_REQ", "case:REG_DATE"], axis=1)
+
+    log = log.rename(columns={
+        "case:concept:name" : "case_id",
+        "concept:name" : "activity_key",
+        "time:timestamp" : "timestamp",
+        "org:resource" : "resource",
+        "lifecycle:transition" : "transition"
+    })
+
+    return log
