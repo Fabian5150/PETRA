323f8f2e18b001db4a9c0e71b3c806f697d5cc91
Fabian Strauch <fabian.strauch.04@web.de>
2026-03-04 16:28:35 +0100

    Blocking post request for running simulation

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,13 @@
+import sys
+from pathlib import Path
 from fastapi import APIRouter, Body
 from fastapi.responses import PlainTextResponse
 
 from services.state_service import load_kpis, load_bpmn_str, store_bpmn_str
+
+# wow, I hate python...
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+from pipeline.simulate_get_kpis import run_pipeline as run_prosimos
 
 router = APIRouter()
 
@@ -17,6 +23,14 @@ def get_current_kpis():
     
     return data
 
+"""
+Updates the current process model and runs prosimos on it
+Blocks response until prosimos is done
+"""
 @router.post("/process-model-simulation")
 def set_current_process_model(bpmnString: str = Body(..., media_type="text/plain")):
     store_bpmn_str(bpmnString)
+
+    run_prosimos()
+
+    return {"message": "Simulation done"}
