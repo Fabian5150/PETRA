91a45ae07e2e4e4574ee7b7de1308017865c08de
Fabian Strauch <fabian.strauch.04@web.de>
2026-03-08 17:57:37 +0100

    Storing optimal path to state after running sim and rl pathfinder

--- pipelines/sim_rl.py
+++ pipelines/sim_rl.py
@@ -3,10 +3,16 @@
 from simulation.prosimos.run_prosimos import run_sim
 from rl_bottlenecks.rl_pipeline import run_q_learning_pipeline
 
+from app.services.state_service import store_optimal_path
+
 def run_pipeline():
     sim_log = run_sim()
 
     optimal_path = run_q_learning_pipeline(sim_log)
+
+    store_optimal_path(optimal_path)
+
+    print("Stored optimal path in state")
 
     
 if __name__ == "__main__":
